83a9054b9b7e7591f2d6cc5ccbae89620d714067
Kelvin Magalhães <mkelvin@live.com>
2023-04-06 17:31:46 -0300

    Cypress studies

---
+++ Cypress-Start/cypress.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "e2e": {
+    "reporter": "mochawesome",
+    "reporterOptions": {
+      "reportDir": "cypress/report/mochawesome-report",
+      "overwrite": true,
+      "html": true,
+      "json": false,
+      "timestamp": "mmddyyyy_HHMMss"
+    }
+  },
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/report/mochawesome-report",
+    "overwrite": true,
+    "html": true,
+    "json": false,
+    "timestamp": "mmddyyyy_HHMMss"
+  },
+  "projectId": "hd1iyf"
+}
