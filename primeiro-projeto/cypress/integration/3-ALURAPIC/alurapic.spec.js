618da01dc371f97c7d98e35adc10f0ceba54877c
Kelvin Magalhães <mkelvin@live.com>
2023-04-12 14:54:18 -0300

    Cypress done

--- primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -1,63 +1,32 @@
-describe('Login e registro de usuarios alura pic', () => {
+describe('Usabilidade da página inicial', () => {
 
     beforeEach(() => {
       cy.visit('https://alura-fotos.herokuapp.com')
 
      })
 
-    it('verifica mensagens validacao', () => {
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
-        cy.contains('button', 'Register').click();
+      /* novos casos de teste */
+    it('verifica mensagens tela inicial', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.get('button[type="submit"]').should('be.disabled');
+ 
     })
 
-    it('verifica mensagens de email invalido', () => {
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="email"]').type('jacqueline');
-        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
-     
-    })
-
-    it('verifica mensagens de senha com menos de 8 caracteres', () => {
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
+    it('verifica botao habilitado na tela inicial', () => {
+        cy.get('input[formcontrolname="userName"]').type('Jacqueline');
         cy.get('input[formcontrolname="password"]').type('123');
-        cy.contains('button', 'Register').click();
-        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
-     
+        cy.get('button[type="submit"]').should('be.enabled');
     })
 
-    it('fazer login de usuario valido', () => {
-        cy.login('flavio', '123');
-        cy.contains('a', '(Logout)').should('be.visible');
+    it('verifica nome da aplicação na tela inicial', () => {
+        cy.contains('a' ,' ALURAPIC ').should('be.visible');
     })
 
-    it('fazer login de usuario invalido', () => {
-        cy.login('jacqueline', '1234');
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('Invalid user name or password');
-        })
+    it('verifica menu clicavel tela inicial', () => {
+        cy.get('.navbar-brand > .fa').click();
+        cy.get('.menu-bar > .fa').should('be.visible');
     })
-
-    
-    const usuarios = require('../../fixtures/usuarios.json');
-    usuarios.forEach(usuario => {
-
-        it(`registra novo usuario ${usuario.userName} `, () => {
-            cy.contains('a', 'Register now').click();
-            cy.contains('button', 'Register').click();
-            cy.get('input[formcontrolname="email"]').type(usuario.email);
-            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
-            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
-            cy.get('input[formcontrolname="password"]').type(usuario.password);
-            cy.contains('button', 'Register').click();
-        })
-    });
 
 
 })
--- primeiro-projeto/cypress/integration/3-ALURAPIC/api-alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/api-alurapic.spec.js
@@ -1,6 +1,8 @@
 describe('Buscar fotos e dados', ()=> {
 
-    it('buscar fotos do flavio', ()=> {
+    it.only('buscar fotos do flavio', ()=> {
+        const tempo = Math.random() - 3000;
+
         cy.request({
             method: 'GET',
             url: 'https://apialurapic.herokuapp.com/flavio/photos'
@@ -9,6 +11,7 @@ describe('Buscar fotos e dados', ()=> {
            expect(res.body).is.not.empty
            expect(res.body[0]).to.have.property('description')
            expect(res.body[0].description).to.be.equal('Farol iluminado') 
+        //    expect(res.duration).to.be.lte(tempo)
         }
 
         )
